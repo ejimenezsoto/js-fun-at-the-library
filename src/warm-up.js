50e60857a816ef5e9d894005de85e3ab54f7650f
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-10-13 21:18:02 -0700

    Complete Warm-up

--- src/warm-up.js
+++ src/warm-up.js
@@ -10,8 +10,8 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
+var title = "Harry Potter";
+var pages = 234;
 
 
 
@@ -19,27 +19,40 @@
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
 
+var pages = pages * 25;
+
+
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie;
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
-
+var sequel = `${title} Strikes Back!!`
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
+var isFavorite = true;
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+
+var isShortStory = true
+
+if(pages > 25 ){
+    isShortStory = false
+} else {
+    pages = true
+}
+
+console.log(isShortStory)
 
 
 
@@ -50,12 +63,24 @@
 // the statement 'I could read that in a day!', otherwise, log the statement 
 // 'Ok, maybe I could read that in a day and a half'
 
+if(pages < 1000){
+    console.log("I could read that in a day!")
+} else {
+    console.log("Ok, maybe I could read that in a day and a half")
+}
+
 
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
+
+if(isFavorite === true){
+    console.log(`My favorite book is ${title}`)
+} else {
+    console.log(`I recently read the ${title} book`)
+}
 
 
 
@@ -64,25 +89,35 @@
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
+var books = ['Dune','Harry Potter','Star Wars','Enders Game','Lord of The Rings']
 
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
+console.log(books[2])
 
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
+var belowTwoHundred = [12,7,87,34,65,11,123,190,33,21,45,78]
 
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(belowTwoHundred[4])
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
+var dictionary = {
+    name: "Webster's",
+    hasThesaurus: false
+}
 
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720;
+console.log(dictionary)
